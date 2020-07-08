71262663c851e07da8e5d853a75becdc09e9e692
Entra21 <entra21@proway.treina>
2020-07-07 21:04:05 -0300

    mudança

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace primeira_aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myText = "salve";
+            int myNumber = 8;
+            float myPrice = 4.099f;
+            bool myBoolean = myPrice == myNumber;
+
+            Console.WriteLine("Você é maior de idade?");
+            var result = Console.ReadLine();
+
+            myBoolean = result == "sim";
+
+            Console.WriteLine(myText);
+            Console.WriteLine(myBoolean);
+            Console.WriteLine(myBoolean);
+        }
+    }
+}
