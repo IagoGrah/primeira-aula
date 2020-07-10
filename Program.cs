453687a5786e7943e7f914fbf2ef5fc583cf727a
Entra21 <entra21@proway.treina>
2020-07-09 21:16:23 -0300

    if nestle

--- Program.cs
+++ Program.cs
@@ -8,9 +8,11 @@ namespace primeira_aula
         {
             Console.WriteLine("Insira sua idade:");
         input:
-            bool number = Int32.TryParse(Console.ReadLine(), out int userAge);
-            if (number == false) {Console.WriteLine("ERRO, insira um número inteiro.");
-            goto input;}
+            if (Int32.TryParse(Console.ReadLine(), out int userAge) == false)
+            {
+                Console.WriteLine("ERRO, insira um número inteiro.");
+                goto input;
+            }
             Console.WriteLine(userAge > 15 && userAge < 51 ? "Pode participar." : "NÃO pode participar.");
         }
     }
